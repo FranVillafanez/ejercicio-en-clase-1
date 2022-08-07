7b22074197001b71b6f3d9a9c406e63c37569919
Francisco Villafáñez <110670253+FranVillafanez@users.noreply.github.com>
2022-08-07 10:37:25 -0300

    ejercicio actualizado

---
+++ Ejercicio para la clase.js
@@ -0,0 +1,4 @@
+let fecha = parseInt(prompt("Ingresar año actual"));
+let nacimiento = parseInt(prompt("Ingresar el año de su nacimiento"));
+let EDAD = fecha - nacimiento + " " + "años es su edad";
+alert(EDAD);
